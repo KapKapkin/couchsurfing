f5991b4416c0f40dc6afa70551dc5c0e8a2848c4
KapKapkin <oleg080505@gmail.com>
2022-06-21 23:42:49 +0300

    db_indexes and image fields

--- accounts/models.py
+++ accounts/models.py
@@ -11,17 +11,26 @@ class CustomUser(AbstractUser):
         primary_key=True, 
         default=uuid.uuid4,
         editable=False,
+        db_index=True,
     )
+
     username = models.CharField(_('username'), max_length=150)
     email = models.CharField(_('Email'), max_length=255, unique=True)
     city = models.CharField(_('City'), max_length=255, null=True, blank=True)
+    avatar = models.FileField(upload_to='avatars/', blank=True)
+    room_image = models.FileField(upload_to='rooms/', blank=True)
     
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['id'], name='id_index')
+        ]
+
     def get_absolute_url(self):
-        return reverse('user_detail', args=[str(self.id)])
+        return reverse('profile', args=[str(self.id)])
 
     def __str__(self):
         return self.username
