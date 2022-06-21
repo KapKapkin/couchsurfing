a2ec831b2316d1843c97d4c1c7008ac2a0f2f926
KapKapkin <oleg080505@gmail.com>
2022-06-21 23:43:16 +0300

    static and media consts

--- couchsurfing/settings.py
+++ couchsurfing/settings.py
@@ -134,6 +134,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_root',
+]
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
