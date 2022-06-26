73a2bef54459c5c8d6aff88a199909d540c169aa
KapKapkin <oleg080505@gmail.com>
2022-06-26 12:12:43 +0300

    admin bug fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-from .forms import (
-    CustomUserCreationForm,
-    CustomUserChangeForm,
-)
+
 
 CustomUser = get_user_model()
 
@@ -13,10 +11,25 @@ CustomUser = get_user_model()
 class CustomUserAdmin(UserAdmin):
     
     model = CustomUser
-    form = CustomUserChangeForm
-    add_form = CustomUserCreationForm
-    list_display = ( 'email', 'first_name', 'last_name', 'city', 'is_staff')
-
+    list_display = ('email', 'name', 'town', 'is_staff')
+    
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("name", "email", "town", "about_me", "avatar", "room_image")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
     
 
 
